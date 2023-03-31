c856ddaf728a11a31b9a3411e73befe4f59dc092
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-03-30 22:51:47 -0700

    change font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['"Sora"', 'sans-serif'],
+        body: ['"Karla"', 'sans-serif'],
+        mono: ['"Jetbrains Mono", mono']
+      }
+    }
   },
   plugins: []
 }
