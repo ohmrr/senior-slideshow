69269a89c820eb6b351cc58988119fb41bcc6e77
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-03-30 22:36:17 -0700

    seems to work now

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,36 @@ interface Student {
 
 function updateCarousel(studentOne: Student, studentTwo: Student) {
   document.querySelector<HTMLDivElement>("#carousel")!.innerHTML = `
-    <img src=".src/assets/images/${studentOne.file}" alt="" class="h-[100vh] w-[50vw] rounded-md shadow-2xl fade-in-out shadow-blue-500/30" />
-    <img src=".src/assets/images/${studentTwo.file}" alt="" class="h-[100vh] w-[50vw] rounded-md shadow-2xl fade-in-out shadow-blue-500/30" />
+  <div class="img-container">
+    <h1 class="absolute heading">${studentOne.name}</h1>
+    <img src="${studentOne.file}" alt="" class="h-full w-[50vw] slide" />
+  </div>
+  <div class="img-container">
+    <h1 class="absolute heading">${studentTwo.name}</h1>
+    <img src="${studentTwo.file}" alt="" class="h-[100vh] w-[50vw] slide" />
+  </div>
   `;
 }
 
-let currentImageIndex = 0;
+let currentIndex = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    currentImageIndex =
-      currentImageIndex + 2 > data.length ? 0 : currentImageIndex + 2;
-    updateCarousel(data[currentImageIndex], data[currentImageIndex - 1]);
+  setInterval(async () => {
+    currentIndex =
+      currentIndex + 2 > data.length ? 0 : currentIndex + 2;
+    
+    const studentOne: Student = {
+      file: new URL(`./assets/images/${data[currentIndex].file}`, import.meta.url).href,
+      name: data[currentIndex].name,
+      college: data[currentIndex].college
+    }
+
+    const studentTwo: Student = {
+      file: new URL(`./assets/images/${data[currentIndex - 1].file}`, import.meta.url).href,
+      name: data[currentIndex - 1].name,
+      college: data[currentIndex - 1].college
+    }
+
+    updateCarousel(studentOne, studentTwo);
   }, 7000);
 });
