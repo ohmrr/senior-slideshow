d49f8b72e26f6380f5a6a17f6ce2c689fcd8e4d0
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-04-02 00:00:18 -0700

    added college portion

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+import './style.css';
 import data from './lib/data.json';
 
 interface Student {
@@ -8,36 +8,48 @@ interface Student {
 }
 
 function updateCarousel(studentOne: Student, studentTwo: Student) {
-  document.querySelector<HTMLDivElement>("#carousel")!.innerHTML = `
+  document.querySelector<HTMLDivElement>('#carousel')!.innerHTML = `
   <div class="img-container">
-    <h1 class="absolute heading">${studentOne.name}</h1>
-    <img src="${studentOne.file}" alt="" class="h-full w-full object-cover col-span-1 max-h-screen slide" />
+    <h1 class="absolute student-name">${studentOne.name}</h1>
+    <img src="${
+      studentOne.file
+    }" alt="" class="h-full w-full object-cover max-h-screen slide" />
+    <div class="bg-white absolute bottom-0 p-4 w-full">
+      <h1 class="student-college">${studentOne.college ? studentOne.college : 'Undecided'}</h1>
+    </div>
   </div>
   <div class="img-container">
-    <h1 class="absolute heading">${studentTwo.name}</h1>
-    <img src="${studentTwo.file}" alt="" class="h-full w-full object-cover col-span-1 max-h-screen slide" />
+    <h1 class="absolute student-name">${studentTwo.name}</h1>
+    <img src="${
+      studentTwo.file
+    }" alt="" class="h-full w-full object-cover max-h-screen slide" />
+    <div class="bg-white absolute bottom-0 p-4 w-full">
+      <h1 class="student-college">${studentTwo.college ? studentTwo.college : 'Undecided'}</h1>
+    </div>
   </div>
   `;
 }
 
 let currentIndex = 0;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   setInterval(async () => {
-    currentIndex =
-      currentIndex + 2 > data.length ? 0 : currentIndex + 2;
-    
+    const nextIndex = (currentIndex + 1) % data.length;
+
     const studentOne: Student = {
       file: new URL(`./assets/images/${data[currentIndex].file}`, import.meta.url).href,
       name: data[currentIndex].name,
-      college: data[currentIndex].college
-    }
+      college: data[currentIndex].college,
+    };
 
     const studentTwo: Student = {
-      file: new URL(`./assets/images/${data[currentIndex - 1].file}`, import.meta.url).href,
-      name: data[currentIndex - 1].name,
-      college: data[currentIndex - 1].college
-    }
+      file: new URL(`./assets/images/${data[nextIndex].file}`, import.meta.url).href,
+      name: data[nextIndex].name,
+      college: data[nextIndex].college,
+    };
+
+    currentIndex = (currentIndex + 2) % data.length;
+    if (currentIndex === data.length - 1 && data.length % 2 === 1) currentIndex = 0;
 
     updateCarousel(studentOne, studentTwo);
   }, 7000);
